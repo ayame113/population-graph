93ab13193c38c1973b23f499d3fdab1001fad68e
ayame113 <40050810+ayame113@users.noreply.github.com>
2022-08-15 19:16:16 +0900

    add server

---
+++ serve.ts
@@ -0,0 +1,10 @@
+import { serve } from "https://deno.land/std@0.152.0/http/server.ts";
+import { serveFile } from "https://deno.land/std@0.152.0/http/file_server.ts";
+import { serveDirWithTs } from "https://deno.land/x/ts_serve@v1.3.0/mod.ts";
+
+serve((request) => {
+  if (new URL(request.url).pathname === "/") {
+    return serveFile(request, "./index.html");
+  }
+  return serveDirWithTs(request);
+});
