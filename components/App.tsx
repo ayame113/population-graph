8fa15cd997e8fd8c22ea45bf611bfd748ebf3f51
ayame113 <40050810+ayame113@users.noreply.github.com>
2022-08-16 21:18:57 +0900

    fix error again

--- components/App.tsx
+++ components/App.tsx
@@ -3,9 +3,9 @@
 // @deno-types="https://esm.sh/v91/@types/react@18.0.17/index.d.ts"
 import React, {
   useState,
-} from "https://esm.sh/v91/react@18.2.0/es2020/react.js#=";
+} from "https://esm.sh/v91/react@18.2.0/es2020/react.js";
 // @deno-types="https://esm.sh/v91/@types/react-dom@18.0.6/index.d.ts"
-import { render } from "https://esm.sh/v91/react-dom@18.2.0/es2020/react-dom.js#=";
+import { render } from "https://esm.sh/v91/react-dom@18.2.0/es2020/react-dom.js";
 
 function App() {
   const [count, setCount] = useState(0);
