e8b742ce4174519822f11320d383e04778e31da4
ayame113 <40050810+ayame113@users.noreply.github.com>
2022-08-16 21:02:41 +0900

    fix runtime error

--- components/App.tsx
+++ components/App.tsx
@@ -1,11 +1,11 @@
 /// <reference lib="dom" />
-///
+
 // @deno-types="https://esm.sh/v91/@types/react@18.0.17/index.d.ts"
 import React, {
   useState,
-} from "https://esm.sh/v91/react@18.2.0/es2022/react.bundle.js";
+} from "https://esm.sh/v91/react@18.2.0/es2020/react.js#=";
 // @deno-types="https://esm.sh/v91/@types/react-dom@18.0.6/index.d.ts"
-import { render } from "https://esm.sh/v91/react-dom@18.2.0/es2022/react-dom.bundle.js";
+import { render } from "https://esm.sh/v91/react-dom@18.2.0/es2020/react-dom.js#=";
 
 function App() {
   const [count, setCount] = useState(0);
