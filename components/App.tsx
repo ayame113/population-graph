8ad0d43ec41c51f9a01fe931874a832aa7fcfbe5
ayame113 <40050810+ayame113@users.noreply.github.com>
2022-08-16 20:04:41 +0900

    fix: pin esm.sh version

--- components/App.tsx
+++ components/App.tsx
@@ -1,6 +1,11 @@
 /// <reference lib="dom" />
-import React, { useState } from "https://esm.sh/react@17.0.2";
-import { render } from "https://esm.sh/react-dom@17.0.2";
+///
+// @deno-types="https://esm.sh/v91/@types/react@18.0.17/index.d.ts"
+import React, {
+  useState,
+} from "https://esm.sh/v91/react@18.2.0/es2022/react.bundle.js";
+// @deno-types="https://esm.sh/v91/@types/react-dom@18.0.6/index.d.ts"
+import { render } from "https://esm.sh/v91/react-dom@18.2.0/es2022/react-dom.bundle.js";
 
 function App() {
   const [count, setCount] = useState(0);
