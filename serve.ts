d893297c70f202e8248fb5779f07f24fb8e7f124
ayame113 <40050810+ayame113@users.noreply.github.com>
2022-08-18 22:34:03 +0900

    first implement for react ssr

--- serve.ts
+++ serve.ts
@@ -4,16 +4,20 @@ import {
   Status,
   STATUS_TEXT,
 } from "https://deno.land/std@0.152.0/http/mod.ts";
-import { serveFile } from "https://deno.land/std@0.152.0/http/file_server.ts";
+import { contentType } from "https://deno.land/std@0.152.0/media_types/mod.ts";
 import { serveDirWithTs } from "https://deno.land/x/ts_serve@v1.3.0/mod.ts";
 
 import type { Routing } from "./types.ts";
 import { init, populationApi, prefecturesApi } from "./server/api.ts";
+import { renderToString } from "./components/Page.tsx";
 
 const routing: Routing[] = [prefecturesApi, populationApi];
 const response404 = new Response(STATUS_TEXT[Status.NotFound], {
   status: Status.NotFound,
 });
+
+const html = renderToString();
+const htmlContentType = contentType("html");
 
 init();
 
@@ -21,7 +25,9 @@ serve(async (request) => {
   // index.htmlのレスポンス
   const url = new URL(request.url);
   if (url.pathname === "/") {
-    return serveFile(request, "./index.html");
+    return new Response(html, {
+      headers: { "Content-Type": htmlContentType },
+    });
   }
 
   // APIへのリクエストを受信
