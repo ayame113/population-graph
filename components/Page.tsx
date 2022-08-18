305bb2b25ea8b9914c0aefcece09e0e77c8da25e
ayame113 <40050810+ayame113@users.noreply.github.com>
2022-08-18 23:30:32 +0900

    cut out a constants

--- components/Page.tsx
+++ components/Page.tsx
@@ -5,6 +5,33 @@ import { Header } from "./Header.tsx";
 import { Main } from "./Main.tsx";
 import { Footer } from "./Footer.tsx";
 
+const preload = [
+  { as: "fetch", href: "/api/prefectures" },
+  { as: "script", href: "/components/PopulationGraph.tsx" },
+  { as: "script", href: "/components/Graph.tsx" },
+  { as: "script", href: "/components/SelectBox.tsx" },
+  { as: "script", href: "/components/deps.ts" },
+  { as: "script", href: "https://esm.sh/v91/react@18.2.0/es2020/react.js" },
+  {
+    as: "script",
+    href: "https://esm.sh/v91/react-dom@18.2.0/es2020/react-dom.js",
+  },
+  {
+    as: "script",
+    href: "https://esm.sh/v91/scheduler@0.23.0/es2020/scheduler.js",
+  },
+  {
+    as: "script",
+    href: "https://esm.sh/v91/react-chartjs-2@4.3.1/es2020/react-chartjs-2.js",
+  },
+  { as: "script", href: "https://esm.sh/v91/chart.js@3.9.1/es2020/chart.js" },
+];
+const TITLE = "都道府県総人口グラフ";
+const DESCRIPTION = "都道府県の人口グラフ。";
+const TOP_PAGE = "https://popl.deno.dev";
+// Copyright 2018 Twitter, Inc and other contributors. Graphics licensed under CC-BY 4.0: https://creativecommons.org/licenses/by/4.0/
+const FAVICON = "https://favi.deno.dev/📈.png";
+
 /** トップページ */
 export function Page() {
   return (
@@ -12,81 +39,28 @@ export function Page() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>都道府県総人口グラフ</title>
+        <title>{TITLE}</title>
         <script type="module" src="/components/PopulationGraph.tsx"></script>
-        <link
-          rel="preload"
-          as="fetch"
-          crossOrigin=""
-          href="/api/prefectures"
-        />
-        <link
-          rel="preload"
-          as="script"
-          crossOrigin=""
-          href="/components/PopulationGraph.tsx"
-        />
-        <link
-          rel="preload"
-          as="script"
-          crossOrigin=""
-          href="/components/Graph.tsx"
-        />
-        <link
-          rel="preload"
-          as="script"
-          crossOrigin=""
-          href="/components/SelectBox.tsx"
-        />
-        <link
-          rel="preload"
-          as="script"
-          crossOrigin=""
-          href="/components/deps.ts"
-        />
-        <link
-          rel="preload"
-          as="script"
-          crossOrigin=""
-          href="https://esm.sh/v91/react@18.2.0/es2020/react.js"
-        />
-        <link
-          rel="preload"
-          as="script"
-          crossOrigin=""
-          href="https://esm.sh/v91/react-dom@18.2.0/es2020/react-dom.js"
-        />
-        <link
-          rel="preload"
-          as="script"
-          crossOrigin=""
-          href="https://esm.sh/v91/scheduler@0.23.0/es2020/scheduler.js"
-        />
-        <link
-          rel="preload"
-          as="script"
-          crossOrigin=""
-          href="https://esm.sh/v91/react-chartjs-2@4.3.1/es2020/react-chartjs-2.js"
-        />
-        <link
-          rel="preload"
-          as="script"
-          crossOrigin=""
-          href="https://esm.sh/v91/chart.js@3.9.1/es2020/chart.js"
-        />
-        <link rel="stylesheet preload" href="/assets/style.css" />
-        <meta name="description" content="都道府県の人口グラフ。" />
+        {preload.map(({ as, href }) => (
+          <link
+            rel="preload"
+            as={as}
+            crossOrigin=""
+            href={href}
+          />
+        ))}
+        <link rel="stylesheet preload" as="style" href="/assets/style.css" />
+        <meta name="description" content={DESCRIPTION} />
         <meta name="theme-color" content="#f32091" />
-        <meta property="og:title" content="都道府県総人口グラフ" />
-        <meta property="og:description" content="都道府県の人口グラフ。" />
-        <meta property="og:url" content="https://popl.deno.dev/" />
-        <meta property="og:image" content="https://favi.deno.dev/📈.png" />
+        <meta property="og:title" content={TITLE} />
+        <meta property="og:description" content={DESCRIPTION} />
+        <meta property="og:url" content={TOP_PAGE} />
+        <meta property="og:image" content={FAVICON} />
         <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@_ayame113_" />
-        {/* Copyright 2018 Twitter, Inc and other contributors. Graphics licensed under CC-BY 4.0: https://creativecommons.org/licenses/by/4.0/ */}
-        <link rel="icon" type="image/png" href="https://favi.deno.dev/📈.png" />
-        <link rel="apple-touch-icon" href="https://favi.deno.dev/📈.png" />
+        <link rel="icon" type="image/png" href={FAVICON} />
+        <link rel="apple-touch-icon" href={FAVICON} />
       </head>
       <body>
         <Header />
